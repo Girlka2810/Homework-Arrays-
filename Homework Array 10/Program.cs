9ce66abbdc4ec62a06005abbc36d99de2accb7ed
Kirill Kireev <kereeevkirill@gmail.com>
2020-10-21 21:32:50 +0300

    ver

---
+++ Homework Array 10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework_Array_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Отсортировать массив по убыванию одним из способов, (отличным от способа в 9-м задании) :  пузырьком(Bubble), выбором (Select) или вставками (Insert))");
+            Console.WriteLine("Введите количество переменных:");
+            Console.Write("N= ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            Random r = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = r.Next(-100, 100);
+            }
+            Console.WriteLine();
+            Console.Write("Исходный массив:");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Отсортированный массив: ");
+            int temp;
+            for (int i = 1; i < a.Length; i++)
+            {
+                temp = a[i];
+                int j = i;
+                while (j > 0 && temp > a[j + 1])
+                {
+                    a[j] = a[j + 1];
+                    j++;
+                    a[j] = temp;
+                }
+                Console.Write(a[i] + " ");
+            }
+
+        }
+    }
+}
+    
---
+++ Homework arrays 8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Homework_arrays_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Поменять местами первую и вторую половину массива, например, для массива 1 2 3 4, результат 3 4 1 2,  или для 12345 - 45312.");
+            Console.WriteLine("Введите количество переменных:");
+            Console.Write("N= ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            Random r = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = r.Next(-100, 100);
+            }
+            Console.WriteLine();
+            Console.Write("Исходный массив:");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Отсортированный массив: ");
+            int h = n / 2;
+            for (int i = 0; i < h; i++)
+            {
+                int t = a[i];
+                if (a.Length % 2 == 0)
+                {
+                    a[i] = a[h + 1];
+                    a[h + 1] = t;
+                }
+                else
+                {
+                    a[i] = a[h + i + 1];
+                    a[h + i + 1] = t;
+                }
+                //if (a.Length % 2 != 0)
+                //{
+                //    a[i] = a[h - 1];
+                //    a[h - 1] = t;
+                //}
+                //else
+                //{
+                //    a[i] = a[h + i - 1];
+                //    a[h + i - 1] = t;
+                //}
+
+                Console.Write(a[i]+" ");
+            }
+        }
+    }
+}
